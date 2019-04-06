243bd443f4533b893478e0e89824d87361024896
tanlin <lintan.spring@gmail.com>
2019-04-06 23:35:06 +0800

    feat: add index module
    modify root mainifest to launch index
    it should has a router
    
    Signed-off-by: tanlin <lintan.spring@gmail.com>

---
+++ index/src/main/java/com/ltan/music/index/IndexActivity.kt
@@ -0,0 +1,21 @@
+package com.ltan.music.index
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * TMusic.com.ltan.music.index
+ *
+ * @ClassName: IndexActivity
+ * @Description:
+ * @Author: tanlin
+ * @Date:   2019/4/6
+ * @Version: 1.0
+ */
+class IndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index)
+    }
+}

701c9152d701ab12fdd6ec956edd192498a113dd
tanlin <lintan.spring@gmail.com>
2019-04-07 12:04:41 +0800

    fix: kotterknife support
    the versions.gradle version code use double quotes
    
    Signed-off-by: tanlin <lintan.spring@gmail.com>

--- ButterKnife-note.md
+++ ButterKnife-note.md
@@ -48,3 +48,40 @@ buildscript {
     }
 }
 ```
+
+### KotterKnife
+Update 04/06/2019
+
+kotlin use `@BindView(R2.id.view_id) lateinit val v: View` directly will NPE
+
+Fortunately, the ButterKnife owner create a new Library [Kotterknife](https://github.com/JakeWharton/kotterknife) base on [butterknife-7.0.1](https://github.com/JakeWharton/butterknife)
+
+```
+// 7.0.1 or higher
+def versions.butterknife = 10.0.0
+implementation "com.jakewharton:butterknife:$versions.butterknife"
+implementation "com.jakewharton:kotterknife:0.1.0-SNAPSHOT"
+```
+
+##### bindView
+the `@BindView` will be replaced by `by bindView`
+```
+// @BindView(R2.id.index_vp)
+// val mViewPager: ViewPager
+
+// this code below is right//
+var mViewPager: ViewPager by bindView(R.id.index_vp)
+```
+##### setValue
+but as you can see, the bindView cannot be support, the IDE marked it in RED, info is
+```
+Missing 'setValue(IndexActivity, KProperty<*>, ViewPager)' method on delegate of type 'ReadOnlyProperty<Activity, ViewPager>'
+```
+
+What ?
+according to the recommendation, generate a fun like this below
+```
+private operator fun Any.setValue(activity: Activity, property: KProperty<*>, v: View) {
+    // just a blank, the error disappeared, so weird...
+}
+```
--- index/src/main/java/com/ltan/music/index/IndexActivity.kt
+++ index/src/main/java/com/ltan/music/index/IndexActivity.kt
@@ -1,11 +1,17 @@
 package com.ltan.music.index
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import butterknife.ButterKnife
-import butterknife.BindView
 import butterknife.Unbinder
+import kotterknife.bindView
+import kotlin.reflect.KProperty
 
 /**
  * TMusic.com.ltan.music.index
@@ -18,8 +24,15 @@ import butterknife.Unbinder
  */
 class IndexActivity : AppCompatActivity() {
 
-    @BindView(R2.id.index_title)
-    lateinit var title: TextView
+    companion object {
+        val TAG = "ltan/Index"
+    }
+
+    var mTitle: TextView by bindView(R.id.index_title)
+
+    // @BindView(R2.id.index_vp)
+    // val mViewPager: ViewPager
+    var mViewPager: ViewPager by bindView(R.id.index_vp)
 
     private var unBinder : Unbinder? = null
 
@@ -27,10 +40,30 @@ class IndexActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index)
         unBinder = ButterKnife.bind(this)
+
+        val inflater = LayoutInflater.from(this)
+        val page1 = inflater.inflate(R.layout.page_1, null, false)
+        val page2 = inflater.inflate(R.layout.page_2, null, false)
+        val page3 = inflater.inflate(R.layout.page_3, null, false)
+        val viewList = ArrayList<View>()
+        viewList.add(page1)
+        viewList.add(page2)
+        viewList.add(page3)
+        for (i in 0..3) {
+            Log.d(TAG, "this is a for loop $i")
+        }
+        val adapter = ViewPagerAdapter(this, viewList)
+        // adapter.setViewList(viewList)
+        mViewPager.adapter = adapter
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unBinder?.unbind()
     }
+}
+
+// so weird....
+private operator fun Any.setValue(activity: Activity, property: KProperty<*>, v: View) {
+
 }
